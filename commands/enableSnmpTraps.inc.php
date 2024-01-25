60fd96ee4131616eb10de4816bc30c5d203a30a3
Andrey Grey <megagramm@gmail.com>
2024-01-25 16:47:32 +0300

    Исправлен ip сервера для получения snmp traps

--- commands/enableSnmpTraps.inc.php
+++ commands/enableSnmpTraps.inc.php
@@ -7,4 +7,4 @@ declare(strict_types=1);
  * @author      Andrey Grey <megagramm@gmail.com>
  */
 $title = "Включить поддержку SNMP Traps";
-$shCommand = "\"echo 'trap2sink 192.168.14.72 public 8062' > /run/etc/snmp/snmpd.d/enableTraps.conf && systemctl restart b4c-snmpd\"";
+$shCommand = "\"echo 'trap2sink 192.168.14.73 public 8062' > /run/etc/snmp/snmpd.d/enableTraps.conf && systemctl restart b4c-snmpd\"";
