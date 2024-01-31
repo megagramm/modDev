e596ca5cc8bec3820135d1d5c190d1e7ec5d832a
Andrey Grey <megagramm@gmail.com>
2024-01-31 21:23:06 +0300

    Добавлены команды перевода файловой системы в режимы rw и ro

---
+++ commands/setFSRO.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @package     modDev
+ * @author      Andrey Grey <megagramm@gmail.com>
+ */
+
+
+$title = "Установить файловую систему в режим записи btrfs-ro";
+$shCommand = <<<EOF
+'btrfs-ro'
+EOF;
