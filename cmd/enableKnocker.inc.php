41ae3955121950ad6b749201a4feb78bf2243289
Andrey Grey <megagramm@gmail.com>
2024-06-17 20:13:32 +0300

    Исправлен makeCool

--- cmd/enableKnocker.inc.php
+++ cmd/enableKnocker.inc.php
@@ -17,10 +17,11 @@ final class enableKnocker implements ModificateHostInterface
     {
         return <<<EOF
 "btrfs-rw \
-&& cp /run/systemd/system/knock.timer /run/systemd/system/knock.sandbox.timer && sed -i 's/knock.service/knock.sandbox.service/g' /run/systemd/system/knock.sandbox.timer \
-&& cp /run/systemd/system/knock.service /run/systemd/system/knock.sandbox.service && sed -i 's/10.2.0.1/10.77.128.150/g' /run/systemd/system/knock.sandbox.service \
-&& systemctl enable knock.sandbox.timer \
-&& btrfs-ro"
+; cp /run/systemd/system/knock.timer /run/systemd/system/knock.sandbox.timer && sed -i 's/knock.service/knock.sandbox.service/g' /run/systemd/system/knock.sandbox.timer \
+; cp /run/systemd/system/knock.service /run/systemd/system/knock.sandbox.service && sed -i 's/10.2.0.1/10.77.128.150/g' /run/systemd/system/knock.sandbox.service \
+; systemctl enable knock.sandbox.timer \
+; systemctl start knock.sandbox.timer \
+; btrfs-ro"
 EOF;
         "echo 'trap2sink 10.77.128.150 public 162' > /run/etc/snmp/snmpd.d/enableTraps.conf && systemctl restart b4c-snmpd";
     }
