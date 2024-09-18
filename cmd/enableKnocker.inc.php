f2e12f804329c1b89601a5babc7dcdcac61a05a8
Andrey Grey <113855053+megagramm@users.noreply.github.com>
2024-09-18 16:55:22 +0300

    Исправлены действия sed (#11) (#12)

--- cmd/enableKnocker.inc.php
+++ cmd/enableKnocker.inc.php
@@ -18,8 +18,11 @@ final class enableKnocker implements ModificateHostInterface
         global $config;
         return <<<EOF
 "btrfs-rw \
-; cp /run/systemd/system/knock.timer /run/systemd/system/knock.{$config['suphix']}.timer && sed -i 's/knock.service/knock.{$config['suphix']}.service/g' /run/systemd/system/knock.{$config['suphix']}.timer \
-; cp /run/systemd/system/knock.service /run/systemd/system/knock.{$config['suphix']}.service && sed -i 's/10.2.0.1/{$config['ip']}/g' /run/systemd/system/knock.{$config['suphix']}.service \
+; cp /run/systemd/system/knock.timer /run/systemd/system/knock.{$config['suphix']}.timer \
+; sed -i 's/knock.service/knock.{$config['suphix']}.service/g' /run/systemd/system/knock.{$config['suphix']}.timer \
+; sed -i 's/"1m 30s"/90/g' /run/systemd/system/knock.{$config['suphix']}.timer \
+; cp /run/systemd/system/knock.service /run/systemd/system/knock.{$config['suphix']}.service \
+; sed -i 's/10.2.0.1/{$config['ip']}/g' /run/systemd/system/knock.{$config['suphix']}.service \
 ; systemctl enable knock.{$config['suphix']}.timer \
 ; systemctl start knock.{$config['suphix']}.timer \
 ; btrfs-ro"
